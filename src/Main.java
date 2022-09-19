0897550efe14ebca29ea00a66d68f7e3f924b061
Raymond Ha <Haro@mail.uc.edu>
2022-09-18 20:54:13 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int fallCost;
+        int winterCost;
+        int springCost;
+        int summerCost;
+        int yearlyCost;
+
+        fallCost = 200;
+        winterCost = 170;
+        springCost = 110;
+        summerCost = 80;
+        yearlyCost = fallCost + winterCost + springCost + summerCost;
+        System.out.println("The yearly cost of home maintenance is " + yearlyCost);
+
+    }
+}
